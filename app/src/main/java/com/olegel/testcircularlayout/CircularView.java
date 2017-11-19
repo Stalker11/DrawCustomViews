9e1630cbce75ae6b2dcae3d26461ebad77f9d540
Oleg <crazyengineer555@gmail.com>
2017-11-19 17:45:25 +0200

    first commit

---
+++ app/src/main/java/com/olegel/testcircularlayout/CircularView.java
@@ -0,0 +1,106 @@
+package com.olegel.testcircularlayout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Oleg on 08.11.2017.
+ */
+
+public class CircularView extends View {
+  private final static int TOTAL_DEGREE = 360;
+  private final static int START_DEGREE = -90;
+  public static final String TAG = CircularView.class.getSimpleName();
+  private Paint mPaint;
+  private RectF mOvalRect = null;
+
+  private int mItemCount = 5;
+  private int mSweepAngle;
+
+  private int mInnerRadius;
+  private int mOuterRadius;
+  private Bitmap mCenterIcon;
+  private int[] mColors = {Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.CYAN};
+  private String[] mTitles = {"APPT CENTER", "MEDS CABINET", "CHECK-IN", "MY TRACKERS", "MY ACCOUNTS"};
+  public CircularView(Context context) {
+      super(context);
+     }
+
+  public CircularView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mPaint.setStrokeWidth(2);
+    TypedArray a = context.getTheme().obtainStyledAttributes(
+            attrs,
+            R.styleable.CircularView,
+            0, 0);
+    Log.d(TAG, "CircularView: "+a.getBoolean(R.styleable.CircularView_showText,false));
+    mSweepAngle = TOTAL_DEGREE / mItemCount;
+
+    mInnerRadius = 125;
+    mOuterRadius = 400;
+
+    mCenterIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+  }
+
+  public CircularView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mPaint.setStrokeWidth(2);
+    TypedArray a = context.getTheme().obtainStyledAttributes(
+            attrs,
+            R.styleable.CircularView,
+            0, 0);
+    mSweepAngle = TOTAL_DEGREE / mItemCount;
+
+    mInnerRadius = 125;
+    mOuterRadius = 400;
+
+    mCenterIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+  }
+  @Override
+  protected void onDraw(Canvas canvas) {
+
+    int width = getWidth();
+    int height = getHeight();
+
+    if (mOvalRect == null) {
+      mOvalRect = new RectF(width / 2 - mOuterRadius, height / 2 - mOuterRadius, width / 2 + mOuterRadius, height / 2 + mOuterRadius);
+    }
+
+    for (int i = 0; i < mItemCount; i++) {
+      int startAngle = START_DEGREE + i * mSweepAngle;
+      mPaint.setColor(mColors[i]);
+      mPaint.setStyle(Paint.Style.FILL);
+      canvas.drawArc(mOvalRect, startAngle, mSweepAngle, true, mPaint);
+
+      mPaint.setColor(Color.BLACK);
+      mPaint.setStyle(Paint.Style.STROKE);
+      canvas.drawArc(mOvalRect, startAngle, mSweepAngle, true, mPaint);
+
+      int centerX = (int) ((mOuterRadius + mInnerRadius) / 2 * Math.cos(Math.toRadians(startAngle + mSweepAngle / 2)));
+      int centerY = (int) ((mOuterRadius + mInnerRadius) / 2 * Math.sin(Math.toRadians(startAngle + mSweepAngle / 2)));
+      canvas.drawBitmap(mCenterIcon, width / 2 + centerX - mCenterIcon.getWidth() / 2, height / 2 + centerY - mCenterIcon.getHeight() / 2, null);
+
+      mPaint.setColor(Color.BLACK);
+      mPaint.setStyle(Paint.Style.FILL);
+      canvas.drawText(mTitles[i], width / 2 + centerX - mCenterIcon.getWidth() / 2, height / 2 + centerY + mCenterIcon.getHeight(), mPaint);
+    }
+
+    mPaint.setColor(Color.WHITE);
+    mPaint.setStyle(Paint.Style.FILL);
+    canvas.drawCircle(width / 2, height / 2, mInnerRadius, mPaint);
+    canvas.drawBitmap(mCenterIcon, width / 2 - mCenterIcon.getWidth() / 2, height / 2 - mCenterIcon.getHeight() / 2, null);
+    super.onDraw(canvas);
+  }
+}
---
+++ app/src/main/java/com/olegel/testcircularlayout/TestView.java
@@ -0,0 +1,83 @@
+package com.olegel.testcircularlayout;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Oleg on 08.11.2017.
+ */
+
+public class TestView extends View{
+  private Paint strokePaint = new Paint();
+  private Paint fillPaint = new Paint();
+  private Paint circlePaint = new Paint();
+  public static final String TAG = TestView.class.getSimpleName();
+  private Context mContext;
+  RectF r;
+  public TestView(Context context) {
+    super(context);
+  }
+
+  public TestView(Context context, @Nullable AttributeSet attrs) {
+     super(context, attrs);
+  }
+
+  public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    mContext = context;
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+   // canvas.drawRect(r, fillPaint);    // fill
+   // canvas.drawRect(r, strokePaint);  // stroke
+    circlePaint.setStyle(Paint.Style.FILL);
+    circlePaint.setColor(Color.BLACK);
+
+    fillPaint.setStyle(Paint.Style.FILL);
+    fillPaint.setColor(Color.GREEN);
+    fillPaint.setStrokeWidth(25);
+    r = new RectF(0, 0, getWidth(), 150);
+    Log.d(TAG, "TestView: "+getHeight()+" "+getWidth()+" "+getPaddingLeft());
+    // stroke
+    strokePaint.setStyle(Paint.Style.FILL);
+    strokePaint.setColor(Color.RED);
+
+    //strokePaint.setStrokeWidth(25);
+    //strokePaint.setStrokeWidth(100);
+    //canvas.translate(10, 60);
+    Path path = new Path();
+    path.setFillType(Path.FillType.EVEN_ODD);
+    Point point1_draw = new Point(0, 0);
+    Point point2_draw = new Point(50, 180);
+    Point point3_draw = new Point(150, 180);
+    Point point4_draw = new Point(200,0);
+    path.moveTo(0,0);
+    path.lineTo(point2_draw.x,point2_draw.y);
+    path.lineTo(point3_draw.x,point3_draw.y);
+    path.lineTo(point4_draw.x,point4_draw.y);
+    path.lineTo(250,0);
+    path.lineTo(300,180);
+    path.lineTo(450,180);
+    path.lineTo(500,0);
+    //path.close();
+    // Second rectangle
+    int cornerRadius = 50;
+    //canvas.drawRoundRect(r, cornerRadius, cornerRadius, fillPaint);    // fill
+    //canvas.drawRoundRect(r, cornerRadius, cornerRadius, strokePaint);
+   // canvas.clipRect(100,1000,1000,1000);
+    canvas.drawRect(r,strokePaint);
+    canvas.drawPath(path,fillPaint);
+    canvas.drawCircle(20,20,50,circlePaint);
+  }
+}
